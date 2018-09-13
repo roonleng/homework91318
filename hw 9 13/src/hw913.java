582cd17ac0c0fc72e8a56ff62e3a22f2730f7ee5
roonleng <42977073+roonleng@users.noreply.github.com>
2018-09-13 13:38:44 -0400

    Add files via upload

---
+++ hw 9 13/src/hw913.java
@@ -0,0 +1,27 @@
+
+public class hw913 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		String name= "Rohit";
+		System.out.print(name);
+		int length= 7;
+		int width= 9000;
+		int area=length*width;
+		System.out.print("Length is");
+		System.out.print(length);
+		System.out.print("Width is");
+		System.out.print(width);
+		System.out.print("Area is");
+		System.out.print(area);
+		int radius= 3;
+		System.out.print("Radius is");
+		System.out.print(radius);
+		int areaCircle=2*radius*radius*3.14;
+		int Circumfrence=
+		
+	}
+
+}
